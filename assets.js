9fe2b6190cea598a5af24d1fe7918a1341c1b192
rajankitdev-del <sinha.ankit.dev@gmail.com>
2026-05-14 12:54:53 +0530

    Delete assets.js

--- assets.js
+++
@@ -1,9 +0,0 @@
-import queueIcon from "../assets/queue.png";
-import userIcon from "../assets/user.png";
-import bellIcon from "../assets/bell.png";
-
-export const Icons = {
-  queue: queueIcon,
-  user: userIcon,
-  bell: bellIcon,
-};
